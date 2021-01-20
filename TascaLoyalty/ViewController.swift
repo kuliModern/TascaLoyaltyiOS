4bb050cd83efcf7bd4cb9226feb51b2d7418f889
azkakusuma <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-01-20 16:37:21 +0700

    Initial Commit

---
+++ TascaLoyalty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TascaLoyalty
+//
+//  Created by Azka Kusuma Edy on 20/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
